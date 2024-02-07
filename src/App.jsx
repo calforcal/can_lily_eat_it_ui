7566d7bce0096e79067f3d9b458a85542084baea
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2024-02-06 17:13:56 -0700

    Add search and results components to main App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,25 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import MainHeading from './MainHeading/MainHeading'
+import MainHeading from '/src/MainHeading/MainHeading'
+import UpcSearchBar from '/src/UpcSearchBar/UpcSearchBar'
+import UpcSearchResults from './UpcSearchResults/UpcSearchResults'
 
 function App() {
+
+  const [result, setResult] = useState();
 
   return (
     <>
       <MainHeading />
+      <UpcSearchBar setResult={setResult} />
+      { result ?
+          <UpcSearchResults result={result}/>
+        :
+        <></>
+      }
     </>
   )
 }
 
-export default App
+export default App;
