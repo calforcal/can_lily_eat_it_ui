2b148cc4613f0d28c2d328d5d024925c30d5f498
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2024-02-09 20:13:47 -0700

    Add skeleton for HomePage component

---
+++ src/HomePage/HomePage.jsx
@@ -0,0 +1,17 @@
+import MainHeading from "../MainHeading/MainHeading";
+import "./HomePage.css"
+
+function HomePage() {
+  return (
+    <>
+      <MainHeading />
+      <button>Login</button>
+      <button>Create Account</button>
+      <a>Image Link 1</a>
+      <a>Image Link 2</a>
+      <a>Skip These for now (limited access)</a>
+    </>
+  );
+}
+
+export default HomePage;
