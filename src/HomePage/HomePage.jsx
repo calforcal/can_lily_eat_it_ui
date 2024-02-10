ee71a79d45ec2726f08da09606c9bb1fbf091b46
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2024-02-09 20:57:47 -0700

    Add layout for the HomePage component, Links and buttons

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -1,15 +1,32 @@
 import MainHeading from "../MainHeading/MainHeading";
+import { Link } from "react-router-dom";
 import "./HomePage.css"
 
 function HomePage() {
   return (
     <>
       <MainHeading />
-      <button>Login</button>
-      <button>Create Account</button>
-      <a>Image Link 1</a>
-      <a>Image Link 2</a>
-      <a>Skip These for now (limited access)</a>
+      <div className="home-page-container">
+        <div className="login-register-container">
+          <button>Login</button>
+          <button>Create Account</button>
+        </div>
+        <div className="demo-profile-container">
+          <div className="demo-profile-1">
+            <img className="mr-pic" src="./src/assets/images/mr-1.jpeg" alt="maggie-rogers" />
+            <p>Demo Profile 1</p>
+          </div>
+          <div className="demo-profile-2">
+            <img className="rr-pic" src="./src/assets/images/rr-1.jpeg" alt="maggie-rogers" />
+            <p>Demo Profile 2</p>
+          </div>
+        </div>
+        <div className="skip-login-container">
+          <Link className="skip-login-button" to="/search">
+            <button>Skip These for now (limited access)</button>
+          </Link>
+        </div>
+      </div>
     </>
   );
 }
