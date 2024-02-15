08aba2b25d5d10ddf497bcf8bb29d3ce8785ad3e
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2024-02-15 16:35:03 -0700

    Add separate HTML and logic for good / bad foods

--- src/ProfilePage/ProfilePage.jsx
+++ src/ProfilePage/ProfilePage.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import "./ProfilePage.css"
 import { useLocation } from "react-router-dom";
 import FoodCard from "../FoodCard/FoodCard";
+import MainHeading from "../MainHeading/MainHeading";
 
 function ProfilePage() {
 
@@ -9,6 +10,8 @@ function ProfilePage() {
   const user_id = location.state;
   const [user, setUser] = useState();
   const [foods, setFoods] = useState();
+  const [badFoods, setBadFoods] = useState();
+  const [goodFoods, setGoodFoods] = useState();
 
   useEffect(() => {
     fetch(`https://27965142-cb65-4b7c-9f97-05e599e7c347.mock.pstmn.io/api/v1/users/${user_id}/foods`)
@@ -24,17 +27,50 @@ function ProfilePage() {
     .catch(err => console.logg(err))
   }, []);
 
+  const getBadFoods = () => {
+    if (foods) {
+      let filtered = foods.filter((food) => food.attributes.lily_eat == false)
+      setBadFoods(filtered);
+    }
+  };
+
+  const getGoodFoods = () => {
+    if (foods) {
+      let filtered = foods.filter((food) => food.attributes.lily_eat == true)
+      setGoodFoods(filtered);
+    }
+  };
+
+  useEffect(() => {
+    getBadFoods();
+    getGoodFoods();
+  }, [foods]);
+
+
   return (
     <>
-      <div className="food-cards">
-        {
-        foods ?
-        foods.map((food) => (
-          <FoodCard result={food} />
-        ))
-        :
-        <h1>Nothing to see</h1>
-        }
+      <MainHeading />
+      <div className="foods-container">
+        <div className="bad-foods-container">
+          <h1 className="bad-foods-title">BAD FOODS</h1>
+          { badFoods ?
+              badFoods.map((food) => (
+                <FoodCard result={food} extraClass={"-profile"} />
+              ))
+            :
+              <h1>Nothing to see</h1>
+          }
+        </div>
+        <div className="good-foods-container">
+          <h1 className="good-foods-title">GOOD FOODS</h1>
+          { goodFoods ?
+              goodFoods.map((food) => (
+                <FoodCard result={food} extraClass={"-profile"}/>
+              ))
+            :
+              <h1>Nothing to see</h1>
+          }
+        </div>
       </div>
     </>
   );
