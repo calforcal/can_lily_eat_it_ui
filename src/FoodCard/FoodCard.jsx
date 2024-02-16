67de34b05b2e86b19f8f65bb4c5aee9abdf43733
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2024-02-15 17:04:37 -0700

    Add save and unsave button to food cards, no functionality yet

--- src/FoodCard/FoodCard.jsx
+++ src/FoodCard/FoodCard.jsx
@@ -11,19 +11,28 @@ function FoodCard({result, extraClass}) {
   }
 
   return (
-    <div className={"food-result-container" + extraClass}>
-      <ul>
-        <li className={"food-item" + extraClass}>{allergen.name}</li>
-        <li className={"ingredients-label" + extraClass}>Ingredients</li>
-        <li className={"food-ingredients" + extraClass}>{allergen.ingredients}</li>
-        <br></br>
-        <li className={"allergens-label" + extraClass}>Allergens Found</li>
-        <li className={"food-allergens-found" + extraClass}>{allergen.allergens}</li>
-        <br></br>
-        <li className={"can-lily-eat-it-label" + extraClass}>Can Lily Eat It?</li>
-        <li className={"food-lily-eat" + extraClass}>{allergen.lilyEat ? "Yes, she can!" : "ABSOLUTELY NOT" }</li>
-      </ul>
-    </div>
+    <>
+      <div className={"food-result-container" + extraClass}>
+        <ul>
+          <li className={"food-item" + extraClass}>{allergen.name}</li>
+          <li className={"ingredients-label" + extraClass}>Ingredients</li>
+          <li className={"food-ingredients" + extraClass}>{allergen.ingredients}</li>
+          <br></br>
+          <li className={"allergens-label" + extraClass}>Allergens Found</li>
+          <li className={"food-allergens-found" + extraClass}>{allergen.allergens}</li>
+          <br></br>
+          <li className={"can-lily-eat-it-label" + extraClass}>Can Lily Eat It?</li>
+          <li className={"food-lily-eat" + extraClass}>{allergen.lilyEat ? "Yes, she can!" : "ABSOLUTELY NOT" }</li>
+        </ul>
+        {
+          extraClass == "-profile"
+          ?
+          <button>Unsave Food</button>
+          :
+          <button>Save Food</button>
+        }
+      </div>
+    </>
   );
 }
 
