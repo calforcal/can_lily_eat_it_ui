cd76a3fd950a7c4f116ac00a45bee7f8dc491488
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2024-02-26 14:31:00 -0700

    Add userId pass through to food card to make POST Users Food request

--- src/SearchPage/SearchPage.jsx
+++ src/SearchPage/SearchPage.jsx
@@ -3,9 +3,12 @@ import { useState } from "react";
 import MainHeading from "../MainHeading/MainHeading";
 import FoodCard from "../FoodCard/FoodCard";
 import UpcSearchBar from "../UpcSearchBar/UpcSearchBar";
+import { useLocation } from "react-router-dom";
 
 function SearchPage() {
 
+  const location = useLocation();
+  const userId = location.state.userId;
   const [result, setResult] = useState();
 
   return (
@@ -14,7 +17,7 @@ function SearchPage() {
       <div className="app-search-container">
         <UpcSearchBar setResult={setResult} />
         { result ?
-            <FoodCard result={result.data} extraClass="-search"/>
+            <FoodCard result={result.data} extraClass="-search" userId={userId} />
           :
           <></>
         }
