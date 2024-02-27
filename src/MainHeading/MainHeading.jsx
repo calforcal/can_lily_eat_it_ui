c991a0b05cbcb3643624cad0b5fe144320c8adef
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2024-02-27 09:10:43 -0700

    Add NavBar component to MainHeading for navigation:

--- src/MainHeading/MainHeading.jsx
+++ src/MainHeading/MainHeading.jsx
@@ -1,10 +1,12 @@
 import "./MainHeading.css"
+import NavBar from "../NavBar/NavBar"
 
 function MainHeading() {
   return (
-    <>
+    <div className="header-container">
       <h1 className="main-header">Can Lily Eat IT?</h1>
-    </>
+      <NavBar/>
+    </div>
   );
 }
 
