e8de25e080857f13cc36b890ba8f5af93daa5751
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2024-03-11 15:28:40 -0600

    Update MainHeading component to pass userId to NavBar

--- src/MainHeading/MainHeading.jsx
+++ src/MainHeading/MainHeading.jsx
@@ -1,12 +1,17 @@
 import "./MainHeading.css"
+import { useState } from "react"
 import NavBar from "../NavBar/NavBar"
+import { useSearchParams } from "react-router-dom";
 
-function MainHeading() {
+function MainHeading({userId}) {
+
   return (
-    <div className="header-container">
-      <h1 className="main-header">Can Lily Eat IT?</h1>
-      <NavBar/>
-    </div>
+    <>
+      <div className="header-container">
+        <h1 className="main-header">Can Lily Eat IT?</h1>
+        <NavBar userId={userId} />
+      </div>
+    </>
   );
 }
 
