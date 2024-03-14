59f24bac31f3519b1dbf0d34a1aa714d7bd6197e
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2024-03-14 16:44:18 -0600

    Add DeleteFood functionality / button to profile because of re-rendering issues with FoodCard

--- src/ProfilePage/ProfilePage.jsx
+++ src/ProfilePage/ProfilePage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { React, useEffect, useState } from "react";
 import "./ProfilePage.css"
 import { useLocation, Link } from "react-router-dom";
 import FoodCard from "../FoodCard/FoodCard";
@@ -12,6 +12,11 @@ function ProfilePage() {
   const [foods, setFoods] = useState();
   const [badFoods, setBadFoods] = useState();
   const [goodFoods, setGoodFoods] = useState();
+  const [deletedItem, setDeletedItem] = useState(0);
+
+  const pleaseRender = () => {
+    setDeletedItem(deletedItem + 1);
+  }
 
   useEffect(() => {
     // fetch(`https://27965142-cb65-4b7c-9f97-05e599e7c347.mock.pstmn.io/api/v1/users/${user_id}/foods`)
@@ -48,6 +53,31 @@ function ProfilePage() {
     getGoodFoods();
   }, [foods]);
 
+  const deleteSavedFood = (user_id, food_id) => {
+    // fetch(`https://27965142-cb65-4b7c-9f97-05e599e7c347.mock.pstmn.io/api/v1/users/${userId}/foods/${allergen.foodId}`, {
+    fetch(`http://127.0.0.1:3000/api/v1/users/${user_id}/foods/${food_id}`, {
+      method: 'DELETE',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+    .then((response) => {
+      if (!response.ok) {
+        alert("Something went wrong, please try again.")
+      } else {
+        return response.json()
+      }
+    })
+    .then((data) => setFoods(data.data))
+    .catch(err => console.log(err))
+  }
+
+  const handleDelete = (user_id, food_id) => {
+    deleteSavedFood(user_id, food_id);
+    console.log("deleted")
+  };
+
   return (
     <>
       <MainHeading userId={user_id}/>
@@ -58,7 +88,10 @@ function ProfilePage() {
           <h1 className="bad-foods-title">BAD FOODS</h1>
           { badFoods ?
               badFoods.map((food) => (
-                <FoodCard result={food} extraClass={"-profile"} userId={user_id} />
+                <>
+                  <FoodCard result={food} extraClass={"-profile"} userId={user_id} />
+                  <button>Unsave Food</button>
+                </>
               ))
             :
               <h1>Nothing to see</h1>
@@ -68,7 +101,10 @@ function ProfilePage() {
           <h1 className="good-foods-title">GOOD FOODS</h1>
           { goodFoods ?
               goodFoods.map((food) => (
-                <FoodCard result={food} extraClass={"-profile"} userId={user_id}/>
+                <>
+                  <FoodCard result={food} extraClass={"-profile"} userId={user_id} />
+                  <button onClick={() => {handleDelete(user_id, food.id)}}>Unsave Food</button>
+                </>
               ))
             :
               <h1>Nothing to see</h1>
