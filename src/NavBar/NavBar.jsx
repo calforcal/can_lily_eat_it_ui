b9d20084dce724623b870af075eb330c7a389189
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2024-03-15 09:39:47 -0600

    Add About Link (WIP) and update Colors

--- src/NavBar/NavBar.jsx
+++ src/NavBar/NavBar.jsx
@@ -13,12 +13,14 @@ function NavBar({userId}) {
         userId ?
         <div className="links-container">
           <Link className="profile-link" to="/profile" state={userId}>Profile</Link>
+          <Link className="about-link">About</Link>
           <Link className="search-link" to="/search" state={userId}>Search</Link>
           <Link className="logout-link" to="/" onClick={() => {handleLogout()}}>Log Out</Link>
         </div>
         :
         <div className="links-container">
           <Link className="logout-link" to="/">Home</Link>
+          <Link className="about-link">About</Link>
           <Link className="search-link" to="/search">Search</Link>
         </div>
       }
