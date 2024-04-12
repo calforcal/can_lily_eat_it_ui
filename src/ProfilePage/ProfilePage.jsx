9e83112a5bbda84f7f7eaba5647c6bf7fef929bf
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2024-04-12 14:41:26 -0600

    Refactor profile page to conditional render good and bad foods based on selection

--- src/ProfilePage/ProfilePage.jsx
+++ src/ProfilePage/ProfilePage.jsx
@@ -3,18 +3,19 @@ import "./ProfilePage.css"
 import { useLocation, Link } from "react-router-dom";
 import FoodCard from "../FoodCard/FoodCard";
 import MainHeading from "../MainHeading/MainHeading";
+import TabButton from "../TabButton/TabButton";
 import CheckBoxForm from "../CheckBoxForm/CheckBoxForm";
+import { render } from "react-dom";
 
 function ProfilePage() {
 
   const location = useLocation();
   const user_id = location.state;
-  const [user, setUser] = useState();
   const [foods, setFoods] = useState();
   const [badFoods, setBadFoods] = useState();
   const [goodFoods, setGoodFoods] = useState();
   const [existingAllergens, setExistingAllergens] = useState([]);
-  const [userAllergens, setUserAllergens] = useState([]);
+  const [selectedFoods, setSelectedFoods] = useState();
 
   useEffect(() => {
     // fetch(`https://27965142-cb65-4b7c-9f97-05e599e7c347.mock.pstmn.io/api/v1/users/${user_id}/foods`)
@@ -28,14 +29,6 @@ function ProfilePage() {
     fetch(`http://127.0.0.1:3000/api/v1/users/${user_id}/allergens`)
     .then(response => response.json())
     .then(data => setExistingAllergens(data.data))
-    .catch(err => console.log(err))
-  }, []);
-
-  useEffect(() => {
-    // fetch(`https://27965142-cb65-4b7c-9f97-05e599e7c347.mock.pstmn.io/api/v1/users/${user_id}`)
-    fetch(`http://127.0.0.1:3000/api/v1/logged_in`)
-    .then(response => response.json())
-    .then(data => setUser(data.data))
     .catch(err => console.log(err))
   }, []);
 
@@ -83,37 +76,67 @@ function ProfilePage() {
     console.log("deleted")
   };
 
+  let renderedFoods = <></>
+
+  if (selectedFoods == "bad") {
+    renderedFoods = (
+      <div className="bad-foods-container">
+        <h1 className="bad-foods-title">BAD FOODS</h1>
+        { (badFoods && badFoods.length != 0) ?
+            badFoods.map((food) => (
+              <>
+                <FoodCard result={food} extraClass={"-profile"} userId={user_id} />
+                <button className="unsave-food-button" onClick={() => {handleDelete(user_id, food.id)}}>Unsave Food</button>
+              </>
+            ))
+          :
+            <h1>Save a Food!</h1>
+        }
+      </div>
+    )
+  }
+  else if (selectedFoods == "good") {
+    renderedFoods = (
+      <div className="good-foods-container">
+        <h1 className="good-foods-title">GOOD FOODS</h1>
+        { (goodFoods && goodFoods.length != 0) ?
+            goodFoods.map((food) => (
+              <>
+                <FoodCard key={food.id} result={food} extraClass={"-profile"} userId={user_id} />
+                <button className="unsave-food-button" onClick={() => {handleDelete(user_id, food.id)}}>Unsave Food</button>
+              </>
+            ))
+          :
+            <h1>Save a Food!</h1>
+        }
+      </div>
+    )
+  }
+  else {
+    renderedFoods = (
+      <>{undefined}</>
+    )
+  }
+
+  function handleSelect(selectedButton) {
+    if (selectedFoods == selectedButton) {
+      setSelectedFoods();
+    }
+    else {
+      setSelectedFoods(selectedButton);
+    }
+  }
+
   return (
     <>
       <MainHeading userId={user_id}/>
       <div className="profile-page">
+        <div className="tab-buttons">
+          <TabButton className="tab-button" buttonName="Bad Foods" isSelected={selectedFoods === "bad"} onSelect={() => handleSelect("bad")} />
+          <TabButton className="tab-button" buttonName="Good Foods" isSelected={selectedFoods === "good"} onSelect={() => handleSelect("good")} />
+        </div>
         <div className="foods-container">
-          <div className="bad-foods-container">
-            <h1 className="bad-foods-title">BAD FOODS</h1>
-            { badFoods ?
-                badFoods.map((food) => (
-                  <>
-                    <FoodCard result={food} extraClass={"-profile"} userId={user_id} />
-                    <button className="unsave-food-button" onClick={() => {handleDelete(user_id, food.id)}}>Unsave Food</button>
-                  </>
-                ))
-              :
-                <h1>Nothing to see</h1>
-            }
-          </div>
-          <div className="good-foods-container">
-            <h1 className="good-foods-title">GOOD FOODS</h1>
-            { goodFoods ?
-                goodFoods.map((food) => (
-                  <>
-                    <FoodCard key={food.id} result={food} extraClass={"-profile"} userId={user_id} />
-                    <button className="unsave-food-button" onClick={() => {handleDelete(user_id, food.id)}}>Unsave Food</button>
-                  </>
-                ))
-              :
-                <h1>Nothing to see</h1>
-            }
-          </div>
+          {renderedFoods}
         </div>
       </div>
     </>
