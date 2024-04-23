7dcde647912ff9c73e1f6d8dab6e722b86a2c605
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2024-04-23 16:23:17 -0600

    Update background-image on About and Home page

--- src/AboutPage/AboutPage.jsx
+++ src/AboutPage/AboutPage.jsx
@@ -10,7 +10,13 @@ function AboutPage() {
       <div className='about-page'>
         <h1 className='about-title'>About Can Lily Eat It?</h1>
         <p className='about-info'>
-          Lily is my niece. 1 of 5 nieces (girl uncle!). She developed an allergy to dairy and soy just after she was born, therefore my sister could no longer eat foods with those allergens and she was having a hard time figuring out what contained these allergens. So I decided to make an app the detects Soy and Dairy in any product we can find a UPC code for, to help my sister safely eat packaged foods!
+          Lily is my niece. 1 of 5 nieces (girl uncle!). She developed an allergy to dairy and soy just after she was born, therefore my sister could no longer eat foods with those allergens and she was having a hard time figuring out what contained these allergens. 
+          <br></br>
+          <br></br>
+          The FDA does not require refined soy products to be listed as an allergen on food products. Other products like gelatin also do not show up in listed allergens, even though they can sometimes contain dairy/soy. 
+          <br></br>
+          <br></br>
+          I've created this app to search a UPC code, and compare its ingredients to a highly sensitive list of common and uncommon ingredients allergens are found in.
         </p>
       </div>
     </>
