72531a212f3da1bace50205e65063c92c53a2651
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2024-04-24 09:09:50 -0600

    Update RegisterPage to add password requirements component

--- src/RegisterForm/RegisterForm.jsx
+++ src/RegisterForm/RegisterForm.jsx
@@ -4,6 +4,9 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import YupPassword from "yup-password"
 import { useForm } from "react-hook-form"
 import "./RegisterForm.css"
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import InfoIcon from '@mui/icons-material/Info';
 
 function RegisterForm({ setFormFilled, setUserData }) {
   const [email, setEmail] = useState();
@@ -73,23 +76,31 @@ function RegisterForm({ setFormFilled, setUserData }) {
   return (
     <>
       <div className="register-form">
-        <div className="register-form-container">
+        <div className="form-container">
           <h1 className="register-title">Register</h1>
           <p className="register-info">Please fill in this form to create an account.</p>
+          <div className="register-form-container">
+            <input {...register("email")} type="text" placeholder="Email" value={email} onChange={(e) => handleRegisterInfo("email", e.target.value)} required />
+            <p>{errors.email?.message}</p>
+            <input {...register("name")} type="text" placeholder="Name" value={name} onChange={(e) => handleRegisterInfo("name", e.target.value)} required />
+            <p>{errors.name?.message}</p>
+            <div className="password-requirements-container">
+              <input {...register("password")} type="password" placeholder="Password" value={password} onChange={(e) => handleRegisterInfo("password", e.target.value)} required />
+              <Tooltip placement="right" title="Password Requirements: 8 characters, 1 uppercase, 1 lowercase, 1 number, 1 special character">
+                <IconButton>
+                  <InfoIcon />
+                </IconButton>
+              </Tooltip>
+            </div>
+            <p>{errors.password?.message}</p>
+            <input {...register("passwordConfirmation")} type="password" placeholder="Password Confirmation" value={passwordConfirmation} onChange={(e) => handleRegisterInfo("passwordConfirmation", e.target.value)} required />
+            <p>{errors.passwordConfirmation?.message}</p>
 
-          <input {...register("email")} type="text" placeholder="Email" value={email} onChange={(e) => handleRegisterInfo("email", e.target.value)} required />
-          <p>{errors.email?.message}</p>
-          <input {...register("name")} type="text" placeholder="Name" value={name} onChange={(e) => handleRegisterInfo("name", e.target.value)} required />
-          <p>{errors.name?.message}</p>
-          <input {...register("password")} type="password" placeholder="Password" value={password} onChange={(e) => handleRegisterInfo("password", e.target.value)} required />
-          <p>{errors.password?.message}</p>
-          <input {...register("passwordConfirmation")} type="password" placeholder="Password Confirmation" value={passwordConfirmation} onChange={(e) => handleRegisterInfo("passwordConfirmation", e.target.value)} required />
-          <p>{errors.passwordConfirmation?.message}</p>
-
+          </div>
           <button className="register-button" onClick={handleSubmit(postUserRegistration)}>Register</button>
         </div>
         <div className="container-login-link">
-        <a href="/login"><p>Already have an account?Sign in</p></a>
+          <a className="already-registered-link" href="/login"><p>Already have an account? Sign in</p></a>
         </div>
       </div>
     </>
